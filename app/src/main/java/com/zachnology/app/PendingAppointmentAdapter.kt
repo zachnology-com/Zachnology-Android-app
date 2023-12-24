8e39a3909d3e854bf388d811ba9a3de970428544
Zach Buchar - Envy <zach@zachnology.com>
2023-12-24 16:56:45 -0600

    Add long press menu to pending appointments page

--- app/src/main/java/com/zachnology/app/PendingAppointmentAdapter.kt
+++ app/src/main/java/com/zachnology/app/PendingAppointmentAdapter.kt
@@ -1,15 +1,21 @@
 package com.zachnology.app
 
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.Color
 import android.text.Html
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.MenuRes
+import androidx.appcompat.widget.PopupMenu
 import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.card.MaterialCardView
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import java.text.SimpleDateFormat
 
 class PendingAppointmentAdapter(private val myCon: Context, private val dataSet: ArrayList<PendingAppointment>) :
@@ -60,9 +66,68 @@ class PendingAppointmentAdapter(private val myCon: Context, private val dataSet:
                 intent.putExtra("id", dataSet[position].appointmentId)
                 startActivity(myCon, intent, null)
         }
+        viewHolder.card.setOnLongClickListener() {
+            dataSet[position].appointmentId?.let { it1 ->
+                showMenu(viewHolder.category, R.menu.pending_appointment_context_menu, viewHolder.card,
+                    it1
+                )
+            }
+            true
+        }
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     override fun getItemCount() = dataSet.size
+
+    private fun showMenu(v: View, @MenuRes menuRes: Int, buttonView: MaterialCardView, appointmentId: String) {
+        val popup = PopupMenu(myCon, v)
+
+        popup.menuInflater.inflate(menuRes, popup.menu)
+
+        popup.setOnMenuItemClickListener { menuItem: MenuItem ->
+            when (menuItem.itemId) {
+                R.id.edit_appointment -> {
+                    val intent = Intent(myCon, EditAppointment::class.java)
+                    intent.putExtra("id", appointmentId)
+                    startActivity(myCon, intent, null)
+                }
+                R.id.delete_appointment -> {
+                    var confirmDialog = MaterialAlertDialogBuilder(
+                        myCon,
+                        R.style.Base_Theme_Zachnology_AlertDialog
+                    )
+                        .setTitle("Delete Appointment?")
+                        .setMessage("Are you sure you want to delete this appointment? This action is irreversible.")
+                        .setIcon(R.drawable.outline_delete_24)
+                        .setNegativeButton(
+                            "Cancel",
+                            DialogInterface.OnClickListener { dialog, which ->
+                                dialog.dismiss()
+                            })
+                        .setPositiveButton(
+                            "Yes",
+                            DialogInterface.OnClickListener { dialog, which ->
+                                var deletingDialog = MaterialAlertDialogBuilder(
+                                    myCon,
+                                    R.style.Base_Theme_Zachnology_AlertDialog
+                                )
+                                    .setView(R.layout.loading)
+                                    .setCancelable(false)
+                                    .show()
+                                AppointmentManager.deletePendingAppointment(myCon, appointmentId, {
+                                    deletingDialog.dismiss()
+                                }, {})
+                            })
+                        .show()
+                    true
+                }
+            }
+            true
+        }
+        popup.setOnDismissListener {
+        }
+        // Show the popup menu.
+        popup.show()
+    }
 
 }
